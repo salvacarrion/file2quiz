d620b44015e66223850c1978207bfd2f66b13e90
salvacarrion <salva.carrion@outlook.com>
2020-04-07 04:02:47 +0200

    Fix args

--- quiz2test/main.py
+++ quiz2test/main.py
@@ -6,29 +6,25 @@ import quiz2test
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-a', '--action', help="shows output", default="parse")
+    parser.add_argument('-a', '--action', help="shows output", choices=['parse', 'read', 'convert2txt'])
     parser.add_argument('-i', '--input', help="File or folder to search for the raw exams", default=None)
     parser.add_argument('-o', '--output', help="File or folder to save for the parsed exams", default=None)
     parser.add_argument('-t', '--token', help="Token used to split questions and answers", default=None)
     parser.add_argument('-s', '--show_correct', help="Show correct answer", default=True, action='store_true')
 
     args = parser.parse_args()
-
-    if args.action:
-
-        if args.action == "parse":
-            input_dir = args.input if args.input else os.path.abspath(os.path.join(os.getcwd(), "raw"))
-            output_dir = args.output if args.output else os.path.abspath(os.path.join(os.path.dirname(input_dir), "parsed"))
-            quiz2test.parser.parse_exams(input_dir, output_dir, args.token)
-
-        elif args.action == "read":
-            quiz = quiz2test.parser.load_quiz(args.input)
-            txt = quiz2test.parser.quiz2txt(quiz, args.show_correct)
-            print(txt)
-        else:
-            raise KeyError("Unknown option")
-
+    if args.action == "parse":
+        input_dir = args.input if args.input else os.path.abspath(os.path.join(os.getcwd(), "raw"))
+        output_dir = args.output if args.output else os.path.abspath(os.path.join(os.path.dirname(input_dir), "parsed"))
+        quiz2test.parser.parse_exams(input_dir, output_dir, args.token)
         print("Done!")
+
+    elif args.action == "read":
+        quiz = quiz2test.parser.load_quiz(args.input)
+        txt = quiz2test.parser.quiz2txt(quiz, args.show_correct)
+        print(txt)
+    else:
+        parser.print_help()
 
 
 if __name__ == '__main__':
